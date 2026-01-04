9ad09fff1df3b7e9d45555d8ea69a8820e1f1923
Tom <linux@rohlik.xyz>
2026-01-04 22:16:18 +0100

    conf: Disable unicorn access logging

--- deploy/gunicorn/gunicorn.conf.py
+++ deploy/gunicorn/gunicorn.conf.py
@@ -8,10 +8,9 @@ workers = int(os.environ.get("GUNICORN_WORKERS", "1"))
 threads = int(os.environ.get("GUNICORN_THREADS", "2"))
 timeout = int(os.environ.get("GUNICORN_TIMEOUT", "60"))
 
-accesslog = os.environ.get("GUNICORN_ACCESSLOG", "-")  # stdout
+# Access logs: set to None to disable, or "-" for stdout, or file path
+accesslog = os.environ.get("GUNICORN_ACCESSLOG", None)  # None = disabled
 errorlog = os.environ.get("GUNICORN_ERRORLOG", "-")    # stderr
 loglevel = os.environ.get("GUNICORN_LOGLEVEL", "warning")
 
 preload_app = True
-
-
